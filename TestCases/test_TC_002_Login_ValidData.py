26ea0e0f6e8c81c43401a2372e823e72b6fd514a
Puja Chatterjee <puja.chatterjee0608@gmail.com>
2021-03-06 23:24:22 +0530

    second commit

--- TestCases/test_TC_002_Login_ValidData.py
+++ TestCases/test_TC_002_Login_ValidData.py
@@ -1,4 +1,5 @@
 from Base import InitiateDriver
+import time
 
 
 def test_Validate_Login_ValidData():
@@ -9,5 +10,12 @@ def test_Validate_Login_ValidData():
     LoginBtn = driver.find_element_by_xpath('//input[@value="Login"]')
     loginTab.click()
     uname.send_keys("test")
+#    driver.implicitly_wait(20)
     Pass.send_keys("test")
+#    driver.set_script_timeout(20)
     LoginBtn.click()
+    time.sleep(10)
+    myacc = driver.find_element_by_xpath('//a[contains(text(), "My Account")]')
+    myacc.click()
+    time.sleep(10)
+    driver.close()
